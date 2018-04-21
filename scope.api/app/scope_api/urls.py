9fe365553695dc606e20b954ade964ed61c612be
Tom <tckerr@gmail.com>
2018-04-21 14:29:10 -0400

    Added static files and REST documentation

--- scope.api/app/scope_api/urls.py
+++ scope.api/app/scope_api/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/diagnostics/', include('diagnostics.urls')),
+    url(r'^docs/', include_docs_urls(title='Scope API'))
 ]
