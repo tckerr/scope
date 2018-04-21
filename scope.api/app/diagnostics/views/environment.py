9a68d7fb2d238c2f243f94579e86d65a4c4e7e74
Tom <tckerr@gmail.com>
2018-04-21 15:24:52 -0400

    Superusers

--- scope.api/app/diagnostics/views/environment.py
+++ scope.api/app/diagnostics/views/environment.py
@@ -1,12 +1,13 @@
 import os
 
+from rest_framework import authentication, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class EnvironmentVariableApiView(APIView):
-    authentication_classes = ()
-    permission_classes = ()
+    authentication_classes = (authentication.TokenAuthentication, authentication.BasicAuthentication)
+    permission_classes = (permissions.IsAdminUser,)
 
     def get(self, request, format=None, env_var=''):
         env_var = os.environ.get(env_var, 'Environment variable \"{}\" not defined'.format(env_var))
