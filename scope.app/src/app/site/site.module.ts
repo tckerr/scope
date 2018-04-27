7fd0407df014188a6ce0ef947b24086f36406e41
Tom <tckerr@gmail.com>
2018-04-26 22:25:11 -0400

    Added angular routing

--- scope.app/src/app/site/site.module.ts
+++ scope.app/src/app/site/site.module.ts
@@ -4,16 +4,30 @@ import {NavigationComponent} from './navigation/navigation.component';
 import {AuthModule} from '../auth/auth.module';
 import {DiagnosticsModule} from '../diagnostics/diagnostics.module';
 import {SharedModule} from '../shared/shared.module';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginFormComponent} from '../auth/login-form/login-form.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'app/login', pathMatch: 'full' },
+    { path: 'app', redirectTo: 'app/login', pathMatch: 'full' },
+    { path: 'app/login', component: LoginFormComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
     imports: [
         SharedModule,
         AuthModule,
         DiagnosticsModule,
+        RouterModule.forRoot(routes, { enableTracing: true })
     ],
     declarations: [
         RootComponent,
-        NavigationComponent
+        NavigationComponent,
+        FooterComponent,
+        PageNotFoundComponent
     ],
     providers: [],
     bootstrap: [RootComponent]
